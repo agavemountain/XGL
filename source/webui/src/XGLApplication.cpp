49c6cf0c16088d70dd27c68ec4134fbe37afd8ab
Joe Turner <joe@agavemountain.com>
2021-09-21 16:04:05 -0500

    Add a simple hello world webpage
    Added a simple hello world webpage using the wt framework,
    complete with the necessary CMake to build the executable.

---
+++ source/webui/src/XGLApplication.cpp
@@ -0,0 +1,33 @@
+//! \file XGLApplication.cpp
+//! \brief Application class
+//!
+//! Copyright (C) 2021  IO Industrial Holdings, LLC
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#include "XGLApplication.h"
+#include <Wt/WText.h>
+#include <Wt/WContainerWidget.h>
+
+/*
+ * The env argument contains information about the new session, and
+ * the initial request. It must be passed to the WApplication
+ * constructor so it is typically also an argument for your custom
+ * application constructor.
+*/
+XGLApplication::XGLApplication(const Wt::WEnvironment &env)
+    : WApplication(env)
+{
+    setTitle("Hello world"); 
+    root()->addWidget(std::make_unique<Wt::WText>("Hello world!")); 
+}
---
+++ source/webui/src/main.cpp
@@ -0,0 +1,40 @@
+//! \file main.cpp
+//! \brief Ye olde program entry point
+//!
+//! Copyright (C) 2021  IO Industrial Holdings, LLC
+//!
+//! This program is free software: you can redistribute it and/or modify
+//! it under the terms of the GNU General Public License as published by
+//! the Free Software Foundation, either version 3 of the License, or
+//! (at your option) any later version.
+//!
+//! This program is distributed in the hope that it will be useful,
+//! but WITHOUT ANY WARRANTY; without even the implied warranty of
+//! MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//! GNU General Public License for more details.
+//!
+//! You should have received a copy of the GNU General Public License
+//! along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#include "XGLApplication.h"
+
+int main(int argc, char **argv)
+{
+  /*
+   * Your main method may set up some shared resources, but should then
+   * start the server application (FastCGI or httpd) that starts listening
+   * for requests, and handles all of the application life cycles.
+   *
+   * The last argument to WRun specifies the function that will instantiate
+   * new application objects. That function is executed when a new user surfs
+   * to the Wt application, and after the library has negotiated browser
+   * support. The function should return a newly instantiated application
+   * object.
+   */
+  return Wt::WRun(argc, argv, [](const Wt::WEnvironment &env) {
+    /*
+     * You could read information from the environment to decide whether
+     * the user has permission to start a new application
+     */
+    return std::make_unique<XGLApplication>(env);
+  });
+}
